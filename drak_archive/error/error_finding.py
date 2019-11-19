5dbb546f5cf144681c3fc1af91ad84e84b98f479
Drakovek <DrakovekMail@gmail.com>
2019-11-19 13:01:09 -0800

    Added ability to search for missing media and DVK files.

--- drak_archive/error/error_finding.py
+++ drak_archive/error/error_finding.py
@@ -1,8 +1,9 @@
+from os import listdir
 from drak_archive.file.dvk_handler import DvkHandler
 
 
 def identical_ids(
-        dvk_directory: str = None,
+        dvk_directories: list = None,
         dvk_handler: DvkHandler = None) -> list:
     """
     Checks for Dvk objects with identical IDs.
@@ -21,7 +22,7 @@ def identical_ids(
         handler = dvk_handler
     else:
         handler = DvkHandler()
-        handler.load_dvks([dvk_directory])
+        handler.load_dvks(dvk_directories)
     handler.sort_dvks("a", True)
     size = handler.get_size()
     for i in range(0, size):
@@ -38,3 +39,69 @@ def identical_ids(
                     located.append(k)
                     identicals.append(dvk_k.get_file())
     return identicals
+
+
+def missing_media(
+        dvk_directories: list = None,
+        dvk_handler: DvkHandler = None) -> list:
+    """
+    Checks for Dvk objects which have missing media files.
+
+    Parameters:
+        dvk_directory (str): Directory from which to search for DVK files.
+            Used if dvk_handler is None
+        dvk_handler (list): DvkHandler with loaded DVK files.
+
+    Returns:
+        list: List of Paths for DVK files with missing linked media files
+    """
+    if dvk_handler is not None:
+        handler = dvk_handler
+    else:
+        handler = DvkHandler()
+        handler.load_dvks(dvk_directories)
+    missing = []
+    handler.sort_dvks("a", True)
+    size = handler.get_size()
+    for i in range(0, size):
+        file = handler.get_dvk_sorted(i).get_media_file()
+        s_file = handler.get_dvk_sorted(i).get_secondary_file()
+        if not file.exists() or (s_file is not None and not s_file.exists()):
+            missing.append(handler.get_dvk_sorted(i).get_file())
+    return missing
+
+
+def missing_dvks(
+        dvk_directories: list = None,
+        dvk_handler: DvkHandler = None) -> list:
+    """
+    Checks for files without corresponding DVK files.
+
+    Parameters:
+        dvk_directory (str): Directory from which to search for DVK files.
+            Used if dvk_handler is None
+        dvk_handler (list): DvkHandler with loaded DVK files.
+
+    Returns:
+        list: List of Paths for files with no corresponding DVK file
+    """
+    if dvk_handler is not None:
+        handler = dvk_handler
+    else:
+        handler = DvkHandler()
+        handler.load_dvks(dvk_directories)
+    missing = []
+    for dir in handler.dvk_directories:
+        for f in listdir(dir.directory_path.absolute()):
+            file = dir.directory_path.joinpath(str(f))
+            if not str(file.absolute()).endswith(".dvk") and not file.is_dir():
+                include = True
+                size = dir.get_size()
+                for i in range(0, size):
+                    if (dir.get_dvk(i).get_media_file() == file
+                            or dir.get_dvk(i).get_secondary_file() == file):
+                        include = False
+                        break
+                if include:
+                    missing.append(file)
+    return missing
--- tests/error/test_error_finding.py
+++ tests/error/test_error_finding.py
@@ -4,6 +4,8 @@ from shutil import rmtree
 from drak_archive.file.dvk import Dvk
 from drak_archive.file.dvk_handler import DvkHandler
 from drak_archive.error.error_finding import identical_ids
+from drak_archive.error.error_finding import missing_dvks
+from drak_archive.error.error_finding import missing_media
 
 
 class TestErrorFinding(unittest.TestCase):
@@ -21,33 +23,43 @@ class TestErrorFinding(unittest.TestCase):
         unittest.TestCase.setUp(self)
         self.test_dir = Path("finding")
         self.test_dir.mkdir(exist_ok=True)
+        self.test_dir.joinpath("file0").touch()
         sub = Path(self.test_dir.joinpath("sub").absolute())
         sub.mkdir(exist_ok=True)
+        # DVK 1
         dvk = Dvk(self.test_dir.joinpath("dvk1.dvk").absolute())
         file = self.test_dir.joinpath("file1.txt")
-        # DVK 1
+        file.touch()
         dvk.set_id("id1")
         dvk.set_title("title1")
         dvk.set_artist("artist")
         dvk.set_page_url("/page/url")
         dvk.set_media_file(file)
+        file = self.test_dir.joinpath("fileSecond.no")
+        file.touch()
+        dvk.set_secondary_file(file)
         dvk.write_dvk()
         # DVK 2
-        file = sub.joinpath("file2.txt")
+        file = sub.joinpath("file2.png")
+        file.touch()
         dvk.set_id("id2")
         dvk.set_title("title2")
         dvk.set_file(sub.joinpath("dvk2.dvk").absolute())
         dvk.set_media_file(file)
+        file = sub.joinpath("second.dmf")
+        dvk.set_secondary_file(file)
         dvk.write_dvk()
         # DVK 3
-        file = sub.joinpath("file3.txt")
+        sub.joinpath("file1.txt").touch()
+        file = sub.joinpath("file3.svg")
         dvk.set_id("id1")
         dvk.set_title("title3")
         dvk.set_file(sub.joinpath("dvk3.dvk").absolute())
         dvk.set_media_file(file)
+        dvk.set_secondary_file(None)
         dvk.write_dvk()
         # DVK 4
-        file = self.test_dir.joinpath("file4.txt")
+        file = self.test_dir.joinpath("file4.ogg")
         dvk.set_title("title4")
         dvk.set_file(self.test_dir.joinpath("dvk4.dvk").absolute())
         dvk.set_media_file(file)
@@ -72,6 +84,39 @@ class TestErrorFinding(unittest.TestCase):
         assert ids[1].name == "dvk3.dvk"
         assert ids[2].name == "dvk4.dvk"
         sub = Path(self.test_dir.joinpath("sub").absolute())
-        assert identical_ids(sub) == []
+        assert identical_ids([sub.absolute()]) == []
         assert identical_ids() == []
-        assert len(identical_ids(self.test_dir.absolute())) == 3
+        assert len(identical_ids([self.test_dir.absolute()])) == 3
+
+    def test_missing_media(self):
+        """
+        Tests the missing_media function.
+        """
+        handler = DvkHandler()
+        handler.load_dvks([self.test_dir.absolute()])
+        missing = missing_media(dvk_handler=handler)
+        assert len(missing) == 3
+        assert missing[0].name == "dvk2.dvk"
+        assert missing[1].name == "dvk3.dvk"
+        assert missing[2].name == "dvk4.dvk"
+        assert missing_media() == []
+        sub = Path(self.test_dir.joinpath("sub").absolute())
+        missing = missing_media([sub.absolute()])
+        assert len(missing) == 2
+        assert missing[0].name == "dvk2.dvk"
+        assert missing[1].name == "dvk3.dvk"
+
+    def test_missing_dvks(self):
+        """
+        Tests the missing_dvks function.
+        """
+        missing = missing_dvks([self.test_dir.absolute()])
+        assert len(missing) == 2
+        assert missing[0].name == "file0"
+        assert missing[1].name == "file1.txt"
+        assert missing_dvks() == []
+        handler = DvkHandler()
+        handler.load_dvks([self.test_dir.joinpath("sub").absolute()])
+        missing = missing_dvks(dvk_handler=handler)
+        assert len(missing) == 1
+        assert missing[0].name == "file1.txt"
