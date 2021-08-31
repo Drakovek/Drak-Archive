019bc3719d9149a03b0832499831b7d904303d48
Drakovek <DrakovekMail@gmail.com>
2021-08-31 11:38:33 -0700

    Added functions for finding sequence errors in Dvks.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ desc = "Utility for loading and handling media files in the DVK file format."
 
 setuptools.setup(
     name="dvk-archive",
-    version="0.10.0",
+    version="0.10.1",
     author="Drakovek",
     author_email="DrakovekMail@gmail.com",
     description=desc,
